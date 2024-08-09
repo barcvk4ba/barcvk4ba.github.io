e38ec29b5e76434aac8112eb85c80b0e4d00a840
AshleyRoll <AshleyRoll@users.noreply.github.com>
2024-08-09 14:15:18 +0000

    jekyll build from Action fede61a7f9eed479483ae49ecdaa1223c4a8b1c9

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "Introduction to DMR",
+        "excerpt":"Many BARC memebers have decided to investigate DMR. This series of articles will form a knoweldge base for members and other new to this part of Ham radio.   ","categories": [],
+        "tags": ["dmr"],
+        "url": "/articles/2020-09-17-dmr-introduction",
+        "teaser": null
+      },{
+        "title": "Saturday Meetings start again",
+        "excerpt":"We have returned to our regular face-to-face meetings at the shack. The first meeting was on 27th June at 1:00PM. As Queensland’s COVID-19 restrictions are eased, we have decided to start meeting again. We will be looking to follow the Scouts lead on social distancing rules. Les, our president will...","categories": [],
+        "tags": [],
+        "url": "/2020/06/23/saturday-meetings-returning.html",
+        "teaser": null
+      },{
+        "title": "Club Day Sat 12 Sept 2020, Rochedale Shack",
+        "excerpt":"We had a fruitful day spent at the shack. There was DMR presentation by Leigh and Laurence, and work on the 70cm repeater noise issue. 70cm Repeater Firstly the 70cm repeater was to test our equipment to see if any of that is faulty or not. Once this is established...","categories": [],
+        "tags": [],
+        "url": "/2020/09/12/club-meeting-report.html",
+        "teaser": null
+      },{
+        "title": "Club Day Sat 26 Sept 2020, Rochedale Shack",
+        "excerpt":"We had a windy day but good to see all the members and two new ones who attended as well around 18 bodies in all. Demonstration of “Allstar” Glen VK4PK demonstrated another analogue digital hybrid radio that uses the world wide PABX system to transfer signals around the globe -...","categories": [],
+        "tags": [],
+        "url": "/2020/09/26/club-meeting-report.html",
+        "teaser": null
+      },{
+        "title": "Club Day Sat 24 Oct 2020, Rochedale Shack",
+        "excerpt":"We had quite a good attendance for the day (20) even though it rained out the afternoon and the Scout Hall was being used. Yes we did survive but it was dampish. Visitors Ray Parrinder a long time member and steam engine punk head attended today to say hello, He...","categories": [],
+        "tags": [],
+        "url": "/2020/10/24/club-meeting-report.html",
+        "teaser": null
+      },{
+        "title": "Club Day Sat 14 Nov 2020, Rochedale Shack",
+        "excerpt":"We had a pretty good day on Saturday with around 20 members in attendance, and just a little taste of heat. Our Les is still in hospital with no change so far and any updates we receive I will pass on directly to you. Following is news on some of...","categories": [],
+        "tags": [],
+        "url": "/2020/11/14/club-meeting-report.html",
+        "teaser": null
+      },{
+        "title": "Club News 20 May 2024",
+        "excerpt":"Latest club news Licence exam session Congratulations to Mark Connolly, Raphael Cendrillon and Chris Thomas on passing their Foundation and Practical exams. A special mention to Michael VK4MWL for successfully delivering his first exam session as accredited facilitator! Thursday Zoom Meeting On Thursday 23 May, Renato VK4TNT is planning a...","categories": [],
+        "tags": [],
+        "url": "/2024/05/20/club-meeting-report.html",
+        "teaser": null
+      },{
+        "title": "Club News 23 July 2024",
+        "excerpt":"Latest club news Marty Nelson of Mad Dog Coils - Club visit Are you interested in getting involved with Parks on the Air and enjoying the great outdoors while playing radio? Are you looking for tips on portable operation? Are you interested in learning how to design, build and test...","categories": [],
+        "tags": [],
+        "url": "/2024/07/23/club-meeting-report.html",
+        "teaser": null
+      },{
+        "title": "VK4PK 6m Flowerpot Antenna",
+        "excerpt":"Glen Lyons, VK4PK, has provided the following design for a 6 meter flowerpot antenna.   VK4PK 6m Flowerpot Antenna - V1.0   ","categories": [],
+        "tags": [],
+        "url": "/projects/2021-01-18-vk4pk-6m-flowerpot-antenna",
+        "teaser": null
+      },{
+        "title": "VK4BXI End fed antennas and the 49:1 and 64:1 ferrite core impedance transformers",
+        "excerpt":"Bob Townsend, VK4BXI, thoughts on end fed antennas and the popular 49:1 and 64:1 ferrite core impedance transformers   VK4BXI End Fed Antenna - V1.0   ","categories": [],
+        "tags": [],
+        "url": "/projects/2023-03-02-vk4bxi-end-fed-antenna",
+        "teaser": null
+      }]
